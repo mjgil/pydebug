97e6c4ee4701569efcb48bb8640bf4c1236de1cc
Malcom Gilbert <malcomgilbert@gmail.com>
2013-10-13 04:25:16 -0700

    add more to test_pydebug

--- test_pydebug.py
+++ test_pydebug.py
@@ -1,4 +1,5 @@
 import pydebug
+import time
 
 
 debug = pydebug.debug("test")
@@ -12,3 +13,13 @@ debug2("even more awesome")
 
 print 1, 2, 3, 4
 debug(1,2,3,4)
+debug({"test": 1})
+debug({"awesome": True}, True, 1, "test")
+debug([], {1,2,3}, {"awesome": True}, True, 1, "test")
+
+time.sleep(0.1)
+debug('should be milliseconds now')
+
+
+time.sleep(1)
+debug('should be seconds now')
