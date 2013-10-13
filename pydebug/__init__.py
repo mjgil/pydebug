4d9b4c16558b4c3a00d91dce0cc72b7bfe90dd64
Malcom Gilbert <malcomgilbert@gmail.com>
2013-10-13 00:24:34 -0700

    move pydebug code to own folder

---
+++ pydebug/__init__.py
@@ -0,0 +1,21 @@
+# _*_ coding: utf-8 _*_
+
+"""
+pydebug
+-----
+
+This module sets up the main interface for all of pydebug
+
+"""
+
+from __future__ import absolute_import
+
+from .lib import *
+
+__title__ = 'pydebug'
+__version__ = '0.0.1'
+# __build__ = 0x000010
+__author__ = 'Malcom Gilbert'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 Malcom Gilbert'
+# __docformat__ = 'markdown'
---
+++ pydebug/lib.py
@@ -0,0 +1,7 @@
+import termcolor
+import os
+import datetime
+import re
+import colorama
+# import sys # for tty?
+
